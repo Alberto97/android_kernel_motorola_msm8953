890512aff2c3b367a6fecddf1b3f86b2c71557ad
Alberto97 <albertop2197@gmail.com>
2017-07-11 02:05:25 +0200

    msm: camera: Optionally include changes for 2017 Motorola devices
    Change-Id: I56180d07688acb6eec14a480ec2d051949f39af8

--- drivers/media/platform/msm/camera_v2/isp/msm_isp40.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp40.c
@@ -600,11 +600,13 @@ static void msm_vfe40_read_irq_status_and_clear(struct vfe_device *vfe_dev,
 
 	*irq_status0 &= vfe_dev->irq0_mask;
 	*irq_status1 &= vfe_dev->irq1_mask;
+#ifdef CONFIG_MSMB_CAMERA_2017
 	if (*irq_status0 &&
 		(*irq_status0 == msm_camera_io_r(vfe_dev->vfe_base + 0x38))) {
 		msm_camera_io_w(*irq_status0, vfe_dev->vfe_base + 0x30);
 		msm_camera_io_w_mb(1, vfe_dev->vfe_base + 0x24);
 	}
+#endif
 
 	if (*irq_status1 & (1 << 0)) {
 		vfe_dev->error_info.camif_status =
@@ -1813,7 +1815,9 @@ static int msm_vfe40_axi_restart(struct vfe_device *vfe_dev,
 	/* Start AXI */
 	msm_camera_io_w(0x0, vfe_dev->vfe_base + 0x2C0);
 
+#ifdef CONFIG_MSMB_CAMERA_2017
 	if (vfe_dev->recovery_irq0_mask || vfe_dev->recovery_irq1_mask)
+#endif
 		msm_vfe40_config_irq(vfe_dev, vfe_dev->recovery_irq0_mask,
 		vfe_dev->recovery_irq1_mask, MSM_ISP_IRQ_SET);
 
--- drivers/media/platform/msm/camera_v2/sensor/msm_sensor.h
+++ drivers/media/platform/msm/camera_v2/sensor/msm_sensor.h
@@ -89,7 +89,9 @@ struct msm_sensor_ctrl_t {
 	uint32_t set_mclk_23880000;
 	uint8_t is_csid_tg_mode;
 	uint32_t is_secure;
+#ifdef CONFIG_MSMB_CAMERA_2017
 	uint8_t bypass_video_node_creation;
+#endif
 	uint32_t no_hw_strobe;
 };
 
