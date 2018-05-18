e607fee93c324a3fe211de1a94f4bb46ff19e078
Veera Venkata Krishna Rao Kolli <cxrm67@motorola.com>
2018-05-18 14:58:45 +0530

    Wifi MAC address different from utags
    Recent QC change of returning on param check fail is conflicting
    with MOTO_UTAGS_MAC implementation. Remove that check here as
    we are anyhow doing it before using it.
    
    Change-Id: I0bf89d08a422f7f2e97bb8ef2deacfcc65a464e6
    Reviewed-on: https://gerrit.mot.com/1177138
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Prashanth Chakravarthy <a19110@motorola.com>
    Submit-Approved: Jira Key

--- drivers/staging/prima/CORE/VOSS/src/vos_nvitem.c
+++ drivers/staging/prima/CORE/VOSS/src/vos_nvitem.c
@@ -1797,8 +1797,18 @@ VOS_STATUS vos_nv_readMultiMacAddress( v_U8_t *pMacAddress,
    v_CONTEXT_t pVosContext= NULL;
 #endif
 
+#ifndef MOTO_UTAGS_MAC
    if((0 == macCount) || (VOS_MAX_CONCURRENCY_PERSONA < macCount) ||
       (NULL == pMacAddress))
+// BEGIN IKSWO-92614
+#else
+   if((0 == macCount) || (VOS_MAX_CONCURRENCY_PERSONA < macCount))
+#endif
+    /* MOTO_UTAGS_MAC specifically calls this func with pMacAddress=NULL for reading from utags and
+       keep it ready in gnvEFSTableV2 so that when this func is called via normal flow with valid
+       pMacAddress mac addresses are copied into pMacAddress.So skip the check since its conflicting
+       with latest QC change to return if these params are invalid, MOTO_UTAGS_MAC any how checks it
+       before using,  END  IKSWO-92614 */
    {
       VOS_TRACE( VOS_MODULE_ID_VOSS, VOS_TRACE_LEVEL_ERROR,
           " Invalid Parameter from NV Client macCount %d, pMacAddress %pK",
