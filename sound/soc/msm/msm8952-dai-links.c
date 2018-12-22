MotorolaMobilityLLC/kernel-msm

ASoC:msm8953:Blanie:Audio:bringup:add dai for ali
ali codec base on albuse,but aif1 is different,
and maybe after evb,there are more hw modification.
so add msm8952_ali_hdmi_dba_dai_link for future.

Change-Id: I24219a794299075334a14d9d7ea766f22e836975
Signed-off-by: liaohj <liaohj@motorola.com>
Reviewed-on: https://gerrit.mot.com/1050561
SME-Granted: SME Approvals Granted
SLTApproved: Slta Waiver
Tested-by: Jira Key
Reviewed-by: Wanlong Zhou <zhouwl@motorola.com>
Reviewed-by: Huosheng Liao <liaohs@motorola.com>
Submit-Approved: Jira Key

sound/soc/msm/msm8952-dai-links.c
@@ -1737,6 +1737,23 @@ static struct snd_soc_dai_link msm8952_albus_hdmi_dba_dai_link[] = {
 	},
 };
 
+static struct snd_soc_dai_link msm8952_ali_hdmi_dba_dai_link[] = {
+	{
+		.name = "MARLEY-HDMI",
+		.stream_name = "MARLEY-HDMI DBA Playback",
+		.cpu_name = "marley-codec",
+		.cpu_dai_name = "marley-aif1", /* ALI use aif1 */
+		.codec_name = "msm-hdmi-dba-codec-rx",
+		.codec_dai_name = "msm_hdmi_dba_codec_rx_dai",
+		.dai_fmt = SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_NF |
+			   SND_SOC_DAIFMT_CBS_CFS,
+		.no_pcm = 1,      /* has a backend */
+		.ignore_pmdown_time = 1,
+		.ignore_suspend = 1,
+		.params = &hdmi_dba_params,
+	},
+};
+
 static struct snd_soc_dai_link msm8952_quin_dai_link[] = {
 	{
 		.name = LPASS_BE_QUIN_MI2S_RX,
@@ -2108,13 +2125,23 @@ struct snd_soc_card *populate_snd_card_dailinks(struct device *dev)
 				__func__);
 			ret = of_property_read_string(dev->of_node,
 				"qcom,dba_cpu_dai_name", &dba_cpu_dai_name);
-			if (ret == 0)
-				msm8952_albus_hdmi_dba_dai_link[0].cpu_dai_name =
-					dba_cpu_dai_name;
-			memcpy(msm8952_dai_links + len4,
-			       msm8952_albus_hdmi_dba_dai_link,
-			       sizeof(msm8952_albus_hdmi_dba_dai_link));
-			len4 += ARRAY_SIZE(msm8952_albus_hdmi_dba_dai_link);
+			if (ret == 0) {
+				if (of_property_read_bool(dev->of_node, "qcom,ali-audio")) {
+					msm8952_ali_hdmi_dba_dai_link[0].cpu_dai_name =
+						dba_cpu_dai_name;
+					memcpy(msm8952_dai_links + len4,
+					       msm8952_ali_hdmi_dba_dai_link,
+					       sizeof(msm8952_ali_hdmi_dba_dai_link));
+					len4 += ARRAY_SIZE(msm8952_ali_hdmi_dba_dai_link);
+				} else {
+					msm8952_albus_hdmi_dba_dai_link[0].cpu_dai_name =
+						dba_cpu_dai_name;
+					memcpy(msm8952_dai_links + len4,
+					       msm8952_albus_hdmi_dba_dai_link,
+					       sizeof(msm8952_albus_hdmi_dba_dai_link));
+					len4 += ARRAY_SIZE(msm8952_albus_hdmi_dba_dai_link);
+				}
+			}
 		}
 	} else {
 		if (!albus_hw) {

Alberto97/android_kernel_motorola_msm8953

sound/soc/msm/msm8952-dai-links.c
@@ -1832,6 +1832,23 @@ static struct snd_soc_dai_link msm8952_albus_hdmi_dba_dai_link[] = {
 	},
 };
 
+static struct snd_soc_dai_link msm8952_ali_hdmi_dba_dai_link[] = {
+	{
+		.name = "MARLEY-HDMI",
+		.stream_name = "MARLEY-HDMI DBA Playback",
+		.cpu_name = "marley-codec",
+		.cpu_dai_name = "marley-aif1", /* ALI use aif1 */
+		.codec_name = "msm-hdmi-dba-codec-rx",
+		.codec_dai_name = "msm_hdmi_dba_codec_rx_dai",
+		.dai_fmt = SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_NF |
+			   SND_SOC_DAIFMT_CBS_CFS,
+		.no_pcm = 1,      /* has a backend */
+		.ignore_pmdown_time = 1,
+		.ignore_suspend = 1,
+		.params = &hdmi_dba_params,
+	},
+};
+
 static struct snd_soc_dai_link msm8952_quin_dai_link[] = {
 	{
 		.name = LPASS_BE_QUIN_MI2S_RX,
@@ -2277,13 +2294,23 @@ struct snd_soc_card *populate_snd_card_dailinks(struct device *dev)
 				__func__);
 			ret = of_property_read_string(dev->of_node,
 				"qcom,dba_cpu_dai_name", &dba_cpu_dai_name);
-			if (ret == 0)
-				msm8952_albus_hdmi_dba_dai_link[0].cpu_dai_name =
-					dba_cpu_dai_name;
-			memcpy(msm8952_dai_links + len5,
-			       msm8952_albus_hdmi_dba_dai_link,
-			       sizeof(msm8952_albus_hdmi_dba_dai_link));
-			len5 += ARRAY_SIZE(msm8952_albus_hdmi_dba_dai_link);
+			if (ret == 0) {
+				if (of_property_read_bool(dev->of_node, "qcom,ali-audio")) {
+					msm8952_ali_hdmi_dba_dai_link[0].cpu_dai_name =
+						dba_cpu_dai_name;
+					memcpy(msm8952_dai_links + len5,
+					       msm8952_ali_hdmi_dba_dai_link,
+					       sizeof(msm8952_ali_hdmi_dba_dai_link));
+					len5 += ARRAY_SIZE(msm8952_ali_hdmi_dba_dai_link);
+				} else {
+					msm8952_albus_hdmi_dba_dai_link[0].cpu_dai_name =
+						dba_cpu_dai_name;
+					memcpy(msm8952_dai_links + len5,
+					       msm8952_albus_hdmi_dba_dai_link,
+					       sizeof(msm8952_albus_hdmi_dba_dai_link));
+					len5 += ARRAY_SIZE(msm8952_albus_hdmi_dba_dai_link);
+				}
+			}
 		}
 	} else {
 		if (!albus_hw) {